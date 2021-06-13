0dcd324c6af335be3b0419520127bff227a7d440
Sam Jenkins <36191075+samrjenkins@users.noreply.github.com>
2021-06-13 21:27:14 +0100

    override default Psych line length (#23)
    * override default Psych line length
    
    * lint
    
    * new method

--- lib/yalphabetize/writer.rb
+++ lib/yalphabetize/writer.rb
@@ -2,6 +2,8 @@
 
 module Yalphabetize
   class Writer
+    MAX_LINE_WIDTH = -1
+
     def initialize(stream_node, path)
       @stream_node = stream_node
       @path = path
@@ -9,12 +11,16 @@ module Yalphabetize
 
     def call
       File.open(path, 'w') do |file|
-        file.write(stream_node.to_yaml)
+        file.write new_file_content
       end
     end
 
     private
 
     attr_reader :stream_node, :path
+
+    def new_file_content
+      stream_node.to_yaml nil, line_width: MAX_LINE_WIDTH
+    end
   end
 end
