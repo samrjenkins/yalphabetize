474b777bf57bf50a1a6a19f6d94d9edaba4d1c7e
Sam Jenkins <36191075+samrjenkins@users.noreply.github.com>
2021-07-11 20:34:27 +0100

    add CLI (#27)
    * add CLI
    
    * alphabetise dependencies
    
    * remove load_path mangling
    
    * update dependencies

---
+++ lib/yalphabetize/cli.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'yalphabetize/alphabetizer'
+require 'yalphabetize/file_yalphabetizer'
+require 'yalphabetize/logger'
+require 'yalphabetize/offence_detector'
+require 'yalphabetize/reader'
+require 'yalphabetize/writer'
+require 'yalphabetize/yaml_finder'
+require 'yalphabetizer'
+
+module Yalphabetize
+  class CLI
+    def self.call(argv)
+      new(argv).call
+    end
+
+    def initialize(argv)
+      @argv = argv
+    end
+
+    def call
+      Yalphabetizer.call(
+        argv,
+        reader_class: Yalphabetize::Reader,
+        finder: Yalphabetize::YamlFinder.new,
+        alphabetizer_class: Yalphabetize::Alphabetizer,
+        writer_class: Yalphabetize::Writer,
+        offence_detector_class: Yalphabetize::OffenceDetector,
+        logger: Yalphabetize::Logger.new($stdout),
+        file_yalphabetizer_class: Yalphabetize::FileYalphabetizer
+      )
+    end
+
+    private
+
+    attr_reader :argv
+  end
+end
