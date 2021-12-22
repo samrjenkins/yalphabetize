8fc7372f9c4a90ca6e0ebe307691624c0659992a
Sam Jenkins <36191075+samrjenkins@users.noreply.github.com>
2021-12-22 19:34:16 +0000

    specify files to inspect in config (#50)

--- spec/yalphabetize/yalphabetizer_spec.rb
+++ spec/yalphabetize/yalphabetizer_spec.rb
@@ -14,20 +14,29 @@ RSpec.describe Yalphabetize::Yalphabetizer do
       end
     end
 
-    context 'with config but config has not configured `excluded`' do
-      include_context 'configuration', 'other_config' => 'foo'
+    context 'with only config' do
+      include_context 'configuration', 'only' => ['foo']
 
-      it 'calls YamlFinder with only and exclude empty' do
-        expect(Yalphabetize::YamlFinder).to receive(:paths).with(only: [], exclude: []).and_return []
+      it 'calls YamlFinder with exclude empty and only with value from config' do
+        expect(Yalphabetize::YamlFinder).to receive(:paths).with(only: ['foo'], exclude: []).and_return []
         subject
+      end
+
+      context 'when file paths provided as args' do
+        let(:args) { ['bar'] }
+
+        it 'calls YamlFinder with only value from args' do
+          expect(Yalphabetize::YamlFinder).to receive(:paths).with(only: ['bar'], exclude: []).and_return []
+          subject
+        end
       end
     end
 
     context 'with excluded config' do
-      include_context 'configuration', 'exclude' => ['bar']
+      include_context 'configuration', 'exclude' => ['baz']
 
       it 'calls YamlFinder with only empty and exclude with value from config' do
-        expect(Yalphabetize::YamlFinder).to receive(:paths).with(only: [], exclude: ['bar']).and_return []
+        expect(Yalphabetize::YamlFinder).to receive(:paths).with(only: [], exclude: ['baz']).and_return []
         subject
       end
     end
