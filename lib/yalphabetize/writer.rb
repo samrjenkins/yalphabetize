9812a150a0b591f50cf9a0ab9942ee5bbfb2099e
Sam Jenkins <36191075+samrjenkins@users.noreply.github.com>
2022-01-04 19:01:47 +0000

    move interpolation replacement to writer (#62)
    * move interpolation replacement to writer
    
    * add interpolations spec

--- lib/yalphabetize/writer.rb
+++ lib/yalphabetize/writer.rb
@@ -4,12 +4,14 @@ module Yalphabetize
   class Writer
     MAX_LINE_WIDTH = -1
 
-    def initialize(stream_node, path)
+    def initialize(stream_node, path, interpolations_mapping)
       @stream_node = stream_node
       @path = path
+      @interpolations_mapping = interpolations_mapping
     end
 
     def call
+      replace_interpolations
       File.open(path, 'w') do |file|
         file.write new_file_content
       end
@@ -17,10 +19,16 @@ module Yalphabetize
 
     private
 
-    attr_reader :stream_node, :path
+    attr_reader :stream_node, :path, :interpolations_mapping
 
     def new_file_content
-      stream_node.to_yaml nil, line_width: MAX_LINE_WIDTH
+      @_new_file_content ||= stream_node.to_yaml(nil, line_width: MAX_LINE_WIDTH)
+    end
+
+    def replace_interpolations
+      interpolations_mapping.each do |uuid, interpolation|
+        new_file_content.sub!(uuid, interpolation)
+      end
     end
   end
 end
--- spec/yalphabetize/writer_spec.rb
+++ spec/yalphabetize/writer_spec.rb
@@ -2,7 +2,7 @@
 
 RSpec.describe Yalphabetize::Writer do
   describe '#call' do
-    subject { Yalphabetize::Writer.new(stream_node, file_path).call }
+    subject { Yalphabetize::Writer.new(stream_node, file_path, interpolations_mapping).call }
 
     let(:file_path) { 'spec/tmp/mock.yml' }
     let(:file) { double('File', write: nil) }
@@ -12,36 +12,63 @@ RSpec.describe Yalphabetize::Writer do
         children: [
           build(
             :document_node,
-            children: [
-              scalar_node
-            ]
+            children: document_children
           )
         ]
       )
     end
-    let(:scalar_node) { build(:scalar_node, value: 'value') }
+    let(:document_children) { [build(:scalar_node, value: 'value')] }
+    let(:interpolations_mapping) { {} }
 
     it 'writes yaml to file' do
-      expect(File).to receive(:open).with(file_path, 'w') do |_, &block|
-        block.call(file)
-      end
-      expect(file).to receive(:write).with("--- value\n")
-
+      expect_to_write "--- value\n"
       subject
     end
 
     context 'when writing yaml that exceeds default psych line length' do
       let(:a_long_string) { ('x ' * 50).strip }
-      let(:scalar_node) { build(:scalar_node, value: a_long_string) }
+      let(:document_children) { [build(:scalar_node, value: a_long_string)] }
 
       it 'does not add extra line breaks' do
-        expect(File).to receive(:open).with(file_path, 'w') do |_, &block|
-          block.call(file)
-        end
-        expect(file).to receive(:write).with("--- #{a_long_string}\n")
-
+        expect_to_write "--- #{a_long_string}\n"
         subject
       end
+    end
+
+    context 'when processing interpolation placeholders' do
+      let(:placeholder1) { SecureRandom.uuid }
+      let(:placeholder2) { SecureRandom.uuid }
+      let(:interpolations_mapping) do
+        {
+          placeholder1 => 'original value 1',
+          placeholder2 => 'original value 2'
+        }
+      end
+      let(:document_children) do
+        [
+          build(
+            :mapping_node,
+            children: [
+              build(:scalar_node, value: 'key1'),
+              build(:scalar_node, value: placeholder1),
+              build(:scalar_node, value: 'key2'),
+              build(:scalar_node, value: placeholder2)
+            ]
+          )
+        ]
+      end
+
+      it 'replaces interpolations placeholders with original values' do
+        expect_to_write("---\nkey1: original value 1\nkey2: original value 2\n")
+        subject
+      end
+    end
+
+    def expect_to_write(content)
+      expect(File).to receive(:open).with(file_path, 'w') do |_, &block|
+        block.call(file)
+      end
+      expect(file).to receive(:write).with(content)
     end
   end
 end
