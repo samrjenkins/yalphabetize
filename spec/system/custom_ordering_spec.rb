1a8bbea5488b90dc120a2cee2f1be7208dadc4fa
Sam Jenkins <sam.jenko@hotmail.com>
2022-02-04 18:10:21 +0000

    add system spec

---
+++ spec/system/custom_ordering_spec.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+RSpec.describe 'custom ordering' do
+  include_context 'configuration', 'allowed_orders' => %w[zero one two three four five six seven eight nine]
+
+  it 'sorts according to the configured order' do
+    expect_offence(<<~YAML)
+      eight: 8
+      five: 5
+      four: 4
+      nine: 9
+      one: 1
+      seven: 7
+      six: 6
+      three: 3
+      two: 2
+      zero: 0
+    YAML
+
+    # expect_reordering(<<~YAML)
+    #   zero: 0
+    #   one: 1
+    #   two: 2
+    #   three: 3
+    #   four: 4
+    #   five: 5
+    #   six: 6
+    #   seven: 7
+    #   eight: 8
+    #   nine: 9
+    # YAML
+  end
+
+  it 'sorts according to the configured order' do
+    expect_offence(<<~YAML)
+      one: 1
+      three: 3
+      two: 2
+      zero: 0
+    YAML
+
+    # expect_reordering(<<~YAML)
+    #   zero: 0
+    #   one: 1
+    #   two: 2
+    #   three: 3
+    # YAML
+  end
+
+  it 'sorts according to the configured order' do
+    expect_no_offences(<<~YAML)
+      one: 1
+      other: foo
+      two: 2
+      zero: 0
+    YAML
+  end
+end
